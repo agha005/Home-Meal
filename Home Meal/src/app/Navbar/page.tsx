9c340e7309c7e91f0aad836a3ee51063a5b7aeb0
Mr - A N A S <mr.anas1003@gmail.com>
2024-07-06 19:57:37 +0500

    responsive navbar

--- Home Meal/src/app/Navbar/page.tsx
+++ Home Meal/src/app/Navbar/page.tsx
@@ -3,20 +3,60 @@ import Link from 'next/link'
 
 export default function Navbar() {
     return (
-     <div className="parent flex mt-5 font-sans text-lg">
-        <div className="logo mx-3">
-            <h1>HOME <span className='text-yellow-400'>MEALS</span></h1>
-        </div>
-        <div className="buttons mx-[24rem] flex gap-4">
-            <a href="#" className='hover:text-yellow-500 translate-all ease-in-out duration-500'>Home</a>
-            <a href="#" className='hover:text-yellow-500 translate-all ease-in-out duration-500'>Menu</a>
-            <a href="#" className='hover:text-yellow-500 translate-all ease-in-out duration-500'>Categories</a>
-            <a href="#" className='hover:text-yellow-500 translate-all ease-in-out duration-500'>About</a>
-        </div>
+        <>
+            <div className="parent w-full h-[80px] flex justify-between align-middle py-5 font-sans sm:px-[80px] px-[40px] text-lg">
+                <div className="logo font-extrabold text-2xl sm:text-xl">
+                    <h1>HOME <span className="text-yellow-400">MEALS</span></h1>
+                </div>
+                <div className="buttons hidden sm:flex gap-4">
+                    <a href="#" className='hover:text-yellow-500 translate-all ease-in-out duration-500'>Home</a>
+                    <a href="#" className='hover:text-yellow-500 translate-all ease-in-out duration-500'>Menu</a>
+                    <a href="#" className='hover:text-yellow-500 translate-all ease-in-out duration-500'>Categories</a>
+                    <a href="#" className='hover:text-yellow-500 translate-all ease-in-out duration-500'>About</a>
+                </div>
 
-        <div className="form mb-4 hover:bg-orange-400 hover:text-black font-bold text-orange-300 border border-orange-300 px-4 py-1 rounded-full cursor-pointer  transition-all ease-in-out duration-500">
-            <h1>Login/SignUp</h1>
-        </div>
-     </div>
-)
+                <div className="form px-3 pb-1 hidden sm:flex justify-center items-center hover:bg-orange-400 hover:text-black font-bold text-orange-300 border border-orange-300 rounded-full cursor-pointer  transition-all ease-in-out duration-500">
+                    <h1>Login/SignUp</h1>
+                </div>
+
+                {/* mobile nav menu  */}
+                <div className="sm:hidden flex flex-col align-middle justify-center gap-1">
+                    <div className="w-6 h-0.5 rounded-full bg-white transition-all duration-200 ease"></div>
+                    <div className="w-6 h-0.5 rounded-full bg-white transition-all duration-200 ease"></div>
+                    <div className="w-6 h-0.5 rounded-full bg-white transition-all duration-200 ease"></div>
+                </div>
+
+            </div>
+
+            {/* mobile complte nav menu  */}
+            <div className="backdrop-blur-lg sm:hidden font-sans p-10 text-[1.4rem] h-[100vh] w-full absolute top-[80px] z-10">
+                <div className="buttons flex flex-col gap-5">
+                    <a href="#" className="hover:text-yellow-500 transition-all ease-in-out duration-500">Home</a>
+                    <a href="#" className="hover:text-yellow-500 transition-all ease-in-out duration-500">Menu</a>
+                    <a href="#" className="hover:text-yellow-500 transition-all ease-in-out duration-500">Categories</a>
+                    <a href="#" className="hover:text-yellow-500 transition-all ease-in-out duration-500">About</a>
+                </div>
+                <div className="form flex justify-center items-center hover:bg-orange-400 hover:text-white font-bold text-orange-300 border border-orange-300 rounded-full cursor-pointer transition-all ease-in-out duration-500 px-4 py-1 mt-10">
+                    <h1>Login/SignUp</h1>
+                </div>
+            </div>
+
+
+
+
+
+
+            {/* <div className="bg-gray-500 sm:hidden font-sans p-10 text-lg h-[90vh] w-full absolute">
+                <div className="buttons flex flex-col gap-5 ">
+                    <a href="#" className='hover:text-yellow-500 translate-all ease-in-out duration-500'>Home</a>
+                    <a href="#" className='hover:text-yellow-500 translate-all ease-in-out duration-500'>Menu</a>
+                    <a href="#" className='hover:text-yellow-500 translate-all ease-in-out duration-500'>Categories</a>
+                    <a href="#" className='hover:text-yellow-500 translate-all ease-in-out duration-500'>About</a>
+                </div>
+                <div className="form w-[150px] flex justify-center align-middle hover:bg-orange-400 hover:text-white font-bold text-orange-300 border border-orange-300 rounded-full cursor-pointer  transition-all ease-in-out duration-500 px-4 py-1 mt-10">
+                    <h1>Login/SignUp</h1>
+                </div>
+            </div> */}
+        </>
+    )
 }
